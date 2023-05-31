73b18038abd0a79a9f45238d64a48ca1945477ca
MCOMPS <mohammadhashmi@graduate.utm.my>
2023-05-31 15:49:05 +0800

    WOW

--- src/Entities/Lecturer.java
+++ src/Entities/Lecturer.java
@@ -1,9 +1,10 @@
 package Entities;
 
-public class Lecturer {
+public class Lecturer extends Person{
     private String staffid;
 
-    public Lecturer(String staffid) {
+    public Lecturer(String name, String staffid) {
+        super(name);
         this.staffid = staffid;
     }
 
